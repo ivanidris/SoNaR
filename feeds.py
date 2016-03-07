75f06c6f3f2e7d021c3345378ab687f81867ecf6
Ivan Idris <ivan.idris@gmail.com>
2016-03-07 21:22:00 +0100

    Storing texts in a corpus. Using multiprocessing for retrieval.

--- feeds.py
+++ feeds.py
@@ -3,51 +3,88 @@ import newspaper
 import pandas as pd
 import dautil as dl
 import datetime
+from multiprocessing_on_dill import Pool
+import listparser
+import os
+import numpy as np
 
 
-urls = ['http://planet.scipy.org/rss20.xml',
-        'http://dsguide.biz/reader/feeds/posts']
-texts = []
-titles = []
-summaries = []
-authors = set()
+def select(arr):
+    return np.percentile(arr, 99.5)
 
 
-log = dl.log_api.conf_logger(__name__)
+def get_terms(alist):
+    df = dl.nlp.calc_tfidf(alist)
 
-for url in urls:
-    rss = fp.parse(url)
+    return dl.nlp.select_terms(df, method=None, select_func=select)
 
-    for entry in rss.entries:
-        titles.append(entry.title)
-        summaries.append(entry.summary)
 
-        try:
-            a = newspaper.Article(entry.link, language='en')
-            a.download()
-            a.parse()
-            texts.append(a.text)
+def init_config():
+    config = newspaper.Config()
+    config.fetch_images = False
+    config.verbose = True
 
-            for author in a.authors:
-                authors.add(author.lower())
+    return config
 
-            log.debug('Link={0} len(text)={1}'.format(
-                entry.link, len(a.text)))
-        except newspaper.article.ArticleException as e:
-            log.warning('{0} {1}'.format(entry.link, e))
 
-text_terms = dl.nlp.select_terms(dl.nlp.calc_tfidf(texts))
-title_terms = dl.nlp.select_terms(dl.nlp.calc_tfidf(titles))
-summary_terms = dl.nlp.select_terms(dl.nlp.calc_tfidf(summaries))
+def process_url(entry):
+    try:
+        if entry.link in corpus.url_set:
+            return
 
-terms = summary_terms.intersection(
-    text_terms.intersection(title_terms)) - authors
+        a = newspaper.Article(entry.link, config)
+        a.download()
+        a.parse()
 
-fname = 'keywords.csv'
-old = set(pd.read_csv(fname)['Term'].values.tolist())
+        corpus.store_text(entry.title.replace(' ', '_'),
+                          a.text, entry.link, entry.title,
+                          " ".join([author.lower()
+                                    for author in a.authors]))
 
-with open(fname, 'a') as csv_file:
-    for t in terms:
-        if t not in old:
-            ts = datetime.datetime.now().isoformat()
-            csv_file.write(ts + ',' + t + ',Use\n')
+        log.debug('Link={0} len(text)={1}'.format(
+            entry.link, len(a.text)))
+    except newspaper.article.ArticleException as e:
+        log.warning('{0} {1}'.format(entry.link, e))
+
+if __name__ == "__main__":
+    urls = ['http://planet.scipy.org/rss20.xml',
+            'http://planetpython.org/rss20.xml',
+            'http://dsguide.biz/reader/feeds/posts']
+
+    for f in os.listdir('opml'):
+        if f.endswith('opml'):
+            fname = os.path.join('opml', f)
+            parsed_opml = listparser.parse(fname)
+            urls.extend([feed.url for feed in parsed_opml.feeds])
+
+    log = dl.log_api.conf_logger(__name__)
+    config = init_config()
+
+    corpus = dl.nlp.WebCorpus('sonar_corpus')
+
+    entries = []
+
+    for url in set(urls):
+        rss = fp.parse(url)
+
+        for entry in set(rss.entries):
+            entries.append(entry)
+
+    with Pool(8) as p:
+        p.map(process_url, entries)
+
+    texts = corpus.get_texts()
+
+    text_terms = get_terms(texts)
+    title_terms = get_terms(corpus.get_titles())
+
+    terms = text_terms.intersection(title_terms) - corpus.get_authors()
+
+    fname = 'keywords.csv'
+    old = set(pd.read_csv(fname)['Term'].values.tolist())
+
+    with open(fname, 'a') as csv_file:
+        for t in terms:
+            if t not in old:
+                ts = datetime.datetime.now().isoformat()
+                csv_file.write(ts + ',' + t + ',Use\n')
